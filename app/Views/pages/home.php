a0a2da4b67b0619da2667f63f6f91b2c297eb00a
Blandin Ilann <blandin.ilann@gmail.com>
2024-04-25 17:28:36 +0200

    match

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -6,6 +6,33 @@
 
 <?= $this->section("main") ?>
     <div id="content">
-
+        <div id="search">
+            <form action="" method="get">
+                <div>
+                    <label for="type" id="type">Type de match</label>
+                    <select name="type" class="search-input">
+                        <option value="">Tous</option>
+                        <option value="1vs1">1vs1</option>
+                        <option value="2vs2">2vs2</option>
+                        <option value="3vs3">3vs3</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="search">Recherche</label>
+                    <input type="text" name="search" placeholder="Equipe, joueur..." class="search-input" id="search">
+                </div>
+            </form>
+        </div>
+        <hr>
+        <div id="matches">
+            <div class="match"></div>
+            <div class="match"></div>
+            <div class="match"></div>
+            <div class="match"></div>
+            <div class="match"></div>
+            <div class="match"></div>
+            <div class="match"></div>
+            <div class="match"></div>
+        </div>
     </div>
 <?= $this->endSection() ?>
