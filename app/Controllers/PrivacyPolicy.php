1d247f3415e3fec36188f322e6435ccba4ab951e
Ilann Blandin <blandin.ilann@gmail.com>
2024-04-29 21:40:20 +0200

    privacy policy

---
+++ app/Controllers/PrivacyPolicy.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class PrivacyPolicy extends BaseController
+{
+    public function view()
+    {
+        return view("pages/privacy_policy");
+    }
+}
+{
+
+}
