0cc278dc9cb3c545b66ef989786c3e6910a41d90
Blandin Ilann <blandin.ilann@gmail.com>
2024-05-07 17:23:42 +0200

    auth

--- app/Controllers/PlayController.php
+++ app/Controllers/PlayController.php
@@ -2,10 +2,7 @@
 
 namespace App\Controllers;
 
-use App\Models\AuthModel;
 use CodeIgniter\Controller;
-use League\OAuth2\Client\Token\AccessToken;
-use MrPropre\OAuth2\Client\Provider\EpicGamesResourceOwner;
 
 class PlayController extends Controller
 {
